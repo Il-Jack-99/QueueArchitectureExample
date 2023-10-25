2b4cd6e22cb9b449fa646e5e4ee884ef6308c2d2
Il-Jack-99 <80536194+Il-Jack-99@users.noreply.github.com>
2023-10-25 09:56:57 +0200

    Queue Example Upload

---
+++ QueueExample/src/test/java/com/queue/queueexample/QueueExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queue.queueexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
